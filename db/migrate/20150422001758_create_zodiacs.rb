177bcd03611aed883286b7bc1dfb456f95573bad
Raghu Betina <rbetina@github.com>
2015-05-04 08:47:45 -0500

    Revert ce8d498..51b3e14
    This rolls back to commit ce8d49889d62df3d6499ca4ebe30f6f84c17ee0e.

--- db/migrate/20150422001758_create_zodiacs.rb
+++
@@ -1,9 +0,0 @@
-class CreateZodiacs < ActiveRecord::Migration
-  def change
-    create_table :zodiacs do |t|
-      t.string :sign
-      t.string :creature
-      t.text :fortune
-    end
-  end
-end
