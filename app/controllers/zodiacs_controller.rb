70b48da831634f48ada192d30f320d6dd1fb26b5
Raghu Betina <rbetina@github.com>
2015-05-04 22:49:30 -0500

    Adds class code

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -1,3 +1,41 @@
 class ZodiacsController < ApplicationController
+  def destroy
 
+    z = Zodiac.find(params["the_id"])
+    z.destroy
+
+    redirect_to("http://localhost:3000/zodiacs")
+  end
+
+
+  def create_row
+    # params = {"symbol"=>"cancer", "animal"=>"crab", "horoscope"=>"good day"}
+
+    z = Zodiac.new
+    z.sign = params["symbol"]
+    z.creature = params["animal"]
+    z.fortune = params["horoscope"]
+    z.save
+
+    redirect_to("http://localhost:3000/zodiacs")
+  end
+
+  def index
+    @list_of_zodiacs = Zodiac.all
+  end
+
+  def sign
+    @zodiac = Zodiac.find_by({ :sign => params["the_sign"] })
+  end
+
+  def creature
+    @zodiac = Zodiac.find_by({ :creature => params["the_creature"] })
+  end
+
+  def show
+    @zodiac = Zodiac.find(params["the_id"])
+  end
+
+  def new_form
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,31 @@ Rails.application.routes.draw do
   get("/lucky_numbers",   { :controller => "fortunes", :action => "lucky" })
   get("/unlucky_numbers", { :controller => "fortunes", :action => "unlucky" })
 
+  get("/random/:min/:max", { :controller => "fortunes", :action => "within_range" })
+
   # VARIABLE ROUTES
   # =======================
 
-  # get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
-  # get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  # Build these again, but use live data from the database this time.
+  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+
+
+
+  get("/zodiacs/new", { :controller => "zodiacs", :action => "new_form" })
+  get("/create_zodiac", { :controller => "zodiacs", :action => "create_row" })
+
+  get("/zodiacs/:the_id",         { :controller => "zodiacs", :action => "show" })
+  get("/zodiacs", { :controller => "zodiacs", :action => "index" })
+
+
+  get("/delete_zodiac/:the_id", { :controller => "zodiacs", :action => "destroy" })
 end
+
+
+
+
+
+
+
+
